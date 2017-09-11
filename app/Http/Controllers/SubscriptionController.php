4ba8d3da8faf2e07ba145ac797ff1df010f23e81
sunny <sunny_kumar@rvtechnologies.co.in>
2017-09-11 12:34:12 +0530

    11-09-2017

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -10,6 +10,7 @@ use App\Subscription;
 use Auth;
 use Stripe\Error\InvalidRequest;
 use Redirect;
+use Mail;
 use View;
 
 class SubscriptionController extends Controller {
@@ -151,7 +152,7 @@ class SubscriptionController extends Controller {
                 'subscription_start' => date('Y-m-d H:i:s', $last_invoice->period->start),
                 'subscription_end' => date('Y-m-d H:i:s', $last_invoice->period->end),
             );
-           // Payment::create($payment);
+            Payment::create($payment);
             // send only in the email
             if ($event_json->data->object->total < 0) {
                 $payment['payment_message'] = "You're account has been successfully downgraded. Your payments and subscription plan information is following.";
