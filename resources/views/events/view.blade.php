026b26541ddb957164a5e7f06c70e3cab56467bc
sunny <sunny_kumar@rvtechnologies.co.in>
2018-03-30 12:46:02 +0530

    30-03-2018

--- resources/views/events/view.blade.php
+++ resources/views/events/view.blade.php
@@ -4,7 +4,7 @@
 <link href="{{ URL::asset('/slick/slick-theme.css') }}" rel="stylesheet">
 @endpush
 @section('content')
-<a href="{{ route('photo.show',$events->event_slug) }}" class="add-photo-button">
+<a href="{{ route('photo.view',$events->event_slug) }}" class="add-photo-button">
                                         <i class="fa fa-camera" aria-hidden="true"></i>Add Photo     
                                     </a>
 
