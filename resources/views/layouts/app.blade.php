380dbc4af13daa2e8819d19d12427307d1fa5615
sunny <sunny_kumar@rvtechnologies.co.in>
2018-04-17 15:41:59 +0530

    17-04-2018

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -241,15 +241,15 @@
             }
         });
         
-         $(".dropdown").hover(function() {
-                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
-                $(this).toggleClass('open');
-                $('b', this).toggleClass("caret caret-up");                
-            },function() {
-                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
-                $(this).toggleClass('open');
-                $('b', this).toggleClass("caret caret-up");                
-            });
+//         $(".dropdown").hover(function() {
+//                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+//                $(this).toggleClass('open');
+//                $('b', this).toggleClass("caret caret-up");                
+//            },function() {
+//                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+//                $(this).toggleClass('open');
+//                $('b', this).toggleClass("caret caret-up");                
+//            });
         @if(!Session::has('latitude') && !Session::has('longitude'))    
             getGeoLocation();
             function getGeoLocation() {
