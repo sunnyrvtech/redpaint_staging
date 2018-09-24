475df891d6b7dc57c8a7484b219e0570cca6b0e2
sunny <sunny_kumar@rvtechnologies.co.in>
2018-09-24 10:53:52 +0530

    24-09-2018

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -216,7 +216,7 @@ dd($lat_long);
     public function getLatLong($country_id, $state, $city, $address, $zip) {
         $country = Country::where('id', $country_id)->first();
         $address = str_replace(" ", "+", $country->name) . "+" . str_replace(" ", "+", $state) . "+" . str_replace(" ", "+", $city) . "+" . str_replace(" ", "+", $address) . "+" . $zip;
-        $url = "http://maps.google.com/maps/api/geocode/json?key=AIzaSyDZGTC412EEKYBmKXxH9VFnE97fKNsu0zQ&address=$address&sensor=false&region=USA";
+        $url = "https://maps.google.com/maps/api/geocode/json?key=AIzaSyDZGTC412EEKYBmKXxH9VFnE97fKNsu0zQ&address=$address&sensor=false&region=USA";
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
