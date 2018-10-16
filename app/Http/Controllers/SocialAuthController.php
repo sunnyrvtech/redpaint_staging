389951d906bc75d37f0861bc19e5c3252903764a
sunny <sunny_kumar@rvtechnologies.co.in>
2018-10-16 10:48:53 +0530

    16-10-2018

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -18,7 +18,7 @@ class SocialAuthController extends Controller {
         $previous_url = explode("/", parse_url(url()->previous(), PHP_URL_PATH));
         
         
-        echo $previous_url;
+        echo url()->previous();
         die;
         
         
