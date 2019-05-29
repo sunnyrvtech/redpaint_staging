dc27f911ed5a3c1d166201fa55b0c3d936bbcceb
sunny <sunny_kumar@rvtechnologies.co.in>
2019-05-29 15:24:20 +0530

    geolocation order check

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -269,7 +269,7 @@
 //                $(this).toggleClass('open');
 //                $('b', this).toggleClass("caret caret-up");                
 //            });
-console.log("{{ Session::has('latitude') }}");
+console.log("{{ Session::get('latitude') }}");
         @if(!Session::has('latitude') && !Session::has('longitude'))    
             getGeoLocation();
             function getGeoLocation() {
